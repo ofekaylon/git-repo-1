c3e88c3ee05b98cdddb4087884ed2ecbfeb68575
ofek <aylonofek@gmail.com>
2020-03-23 10:08:23 +0200

    basic 1

---
+++ basic 1.py
@@ -0,0 +1,6 @@
+from builtins import print, input
+a = "amit"
+name = input("Hi person whats your name?\nEnter your name here:")
+if name == a:
+    print("stop leaving your shit in my room!!".upper())
+else: print("welcome " +name +"!")
